a878fd3931c83f3cd6201fb739d3754471581aa1
Konstantin Trifonov <trifonovkv@gmail.com>
2020-05-06 16:24:02 +0500

    ref: sort sources

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-gdbus-codegen --interface-prefix com.github.trifonovkv --generate-c-code themes-switcher com.github.trifonovkv.themes-switcher.xml
+
+cd service
+
+gdbus-codegen --interface-prefix com.github.trifonovkv \
+    --generate-c-code themes-switcher com.github.trifonovkv.themes-switcher.xml
 
 gcc -o themes-switcher-service themes-switcher-service.c \
-                                themes-switcher.h \
-                                themes-switcher.c \
-                                `pkg-config --cflags --libs gio-2.0 gio-unix-2.0 dbus-1 dbus-glib-1`
+          themes-switcher.h \
+          themes-switcher.c \
+          `pkg-config --cflags --libs gio-2.0 gio-unix-2.0 dbus-1 dbus-glib-1`
 
 sudo kill -9 `pidof /usr/bin/themes-switcher-service`
 sudo mv themes-switcher-service /usr/bin/
